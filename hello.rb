8ed4e0461959b3a17c5367ad772775c360c6a341
wecalderonc <wecalderonc@unal.edu.co>
2019-07-09 17:08:04 -0500

    add functionality

--- hello.rb
+++ hello.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
+# external dependencies
+require 'rack'
+require 'tilt'
+require "rack/protection"
+
+# stdlib dependencies
+require 'thread'
+require 'time'
+require 'uri'
+
 
 set :bind, '0.0.0.0'
 
-get '/' do
-  "Hello World #{params[:name]}".strip
+post '/' do
+  p input = JSON.parse(request.body.read)
+  list(input)
+  #list(JSON.parse(request.body.read))
+  # push = JSON.parse(request.body.read)
+  # list(push)
 end
 
 
@@ -11,11 +25,12 @@ def list names
   if names.size.eql?(0)
     ""
   elsif names.size.eql?(1)
-    names[0][:name]
+    names[0]["name"]
   else
-    array_first_names = names.map { |name| name[:name] }
+    array_first_names = names.map { |name| name["name"] }
     names.first(array_first_names.size - 1).map do |name|
-      p name[:name]
+      p name["name"]
     end.join(", ") + " & " + array_first_names[-1]
   end
 end
+
